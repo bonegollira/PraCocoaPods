2843465fe6768c1417f3680ed8df94d89db66c37
daisuke <banana0928@daisuke-MacBookAir.local>
2014-01-30 18:48:33 +0900

    Initial Commit

---
+++ CocoaPodsLibrary/CocoaPodsLibrary/CocoaPodsLibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodsLibrary
+//
+//  Created by daisuke on 2014/01/30.
+//  Copyright (c) 2014年 daisuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodsLibrary/CocoaPodsLibrary/CocoaPodsLibraryTests/CocoaPodsLibraryTests.m
@@ -0,0 +1,34 @@
+//
+//  CocoaPodsLibraryTests.m
+//  CocoaPodsLibraryTests
+//
+//  Created by daisuke on 2014/01/30.
+//  Copyright (c) 2014年 daisuke. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaPodsLibraryTests : XCTestCase
+
+@end
+
+@implementation CocoaPodsLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
